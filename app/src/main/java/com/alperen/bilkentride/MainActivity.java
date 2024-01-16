804e2c9f9778ea1ce847d299a5d949bc2bcb1241
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-16 22:34:20 +0300

    Setting up Binding settings
    Just little touches

--- app/src/main/java/com/alperen/bilkentride/MainActivity.java
+++ app/src/main/java/com/alperen/bilkentride/MainActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.alperen.bilkentride.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+    private ActivityMainBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
     }
+
+
+
 
 
 
