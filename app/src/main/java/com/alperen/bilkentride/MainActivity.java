9d92cf25cfae89a22f966fe2d5c582f1518fade7
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-16 18:23:35 +0300

    Initial commit

---
+++ app/src/main/java/com/alperen/bilkentride/MainActivity.java
@@ -0,0 +1,19 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Bismillah");
+    }
+}
