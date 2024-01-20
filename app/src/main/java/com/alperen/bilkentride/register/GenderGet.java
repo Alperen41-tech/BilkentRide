34c49a00b5f323f9f85040ed2489521bedaa1842
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-21 01:50:48 +0300

    Xml
    email finished, gender started

---
+++ app/src/main/java/com/alperen/bilkentride/register/GenderGet.java
@@ -0,0 +1,16 @@
+package com.alperen.bilkentride.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alperen.bilkentride.R;
+
+public class GenderGet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gender_get);
+    }
+}
