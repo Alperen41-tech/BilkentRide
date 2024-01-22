91e93ec565f8160ebe8fb536b1ed0ffd49122046
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-22 10:26:03 +0300

    XML's
    DepartmentGet, RiderityGet, PasswordGet, CongratsPage, (Register Page is done) Sign In page are all done, some little touches may be required

---
+++ app/src/main/java/com/alperen/bilkentride/register/RiderityGet.java
@@ -0,0 +1,16 @@
+package com.alperen.bilkentride.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alperen.bilkentride.R;
+
+public class RiderityGet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riderity_get);
+    }
+}
