0574ffc02bd1ae71db97f4bc3deacd21cb4c0cf7
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-26 19:01:26 +0300

    XML
    finding_ring_and_ride_page xml is corrected (Layout problems) + empty button methods are added

--- app/src/main/java/com/alperen/bilkentride/FindingRingAndRidePage.java
+++ app/src/main/java/com/alperen/bilkentride/FindingRingAndRidePage.java
@@ -11,4 +11,34 @@ public class FindingRingAndRidePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finding_ring_and_ride_page);
     }
+
+    public void makeRideButtonClicked()
+    {
+
+    }
+
+    public void findMeRideButtonClicked()
+    {
+
+    }
+
+    public void tellMeRingOptionButtonClicked()
+    {
+
+    }
+
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
 }
