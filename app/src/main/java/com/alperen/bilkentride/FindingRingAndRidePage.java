83c14400893b56f1cf5ef3760e36bf09dd77013b
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-26 17:49:57 +0300

    XML's
    Problems at XML's are done

---
+++ app/src/main/java/com/alperen/bilkentride/FindingRingAndRidePage.java
@@ -0,0 +1,14 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindingRingAndRidePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finding_ring_and_ride_page);
+    }
+}
