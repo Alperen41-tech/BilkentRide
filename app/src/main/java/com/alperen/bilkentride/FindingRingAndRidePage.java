9f2c093719aed6eb139e631e79786813f521f5c1
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2024-01-27 02:11:48 +0300

    Signing In
    Signing in now available, page's backhand is all done

--- app/src/main/java/com/alperen/bilkentride/FindingRingAndRidePage.java
+++ app/src/main/java/com/alperen/bilkentride/FindingRingAndRidePage.java
@@ -2,14 +2,22 @@ package com.alperen.bilkentride;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FindingRingAndRidePage extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finding_ring_and_ride_page);
+
+
     }
 
     public void makeRideButtonClicked()
@@ -24,6 +32,12 @@ public class FindingRingAndRidePage extends AppCompatActivity {
 
     public void tellMeRingOptionButtonClicked()
     {
+
+
+
+
+
+
 
     }
 
--- app/src/main/java/com/alperen/bilkentride/MainActivity.java
+++ app/src/main/java/com/alperen/bilkentride/MainActivity.java
@@ -1,15 +1,23 @@
 package com.alperen.bilkentride;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
+import com.alperen.bilkentride.Classes.Utilities;
 import com.alperen.bilkentride.databinding.ActivityMainBinding;
 import com.alperen.bilkentride.register.BirthdayGet;
 import com.alperen.bilkentride.register.DepartmentGet;
 import com.alperen.bilkentride.register.EmailGet;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
 
+    private FirebaseAuth my_auth;
 
-
+    private SharedPreferences sharing;
 
 
 
@@ -30,11 +39,59 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Utilities.animate_car(this, binding.mainGround);
 
+        my_auth = FirebaseAuth.getInstance();
+
+        sharing = MainActivity.this.getSharedPreferences("com.alperen.bilkentride", MODE_PRIVATE);
+        boolean remembered = sharing.getBoolean("isRemembered", false);
+
+        FirebaseUser user = my_auth.getCurrentUser();
+
+
+
+        if (user != null && remembered){
+            System.out.println("Buraddaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            Intent intent = new Intent(MainActivity.this, FindingRingAndRidePage.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
 
     public void signInButtonClicked(View view){
+
+        String email = binding.emailInputText4.getText().toString();
+        String password = binding.passwordInputText4.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()){
+            Utilities.showToast(this, "Please enter both email and password");
+        }
+
+        else{
+            my_auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Utilities.showToast(MainActivity.this, "Signed In");
+
+                    SharedPreferences sharing_2 = MainActivity.this.getSharedPreferences("com.alperen.bilkentride",
+                            MODE_PRIVATE);
+
+                    sharing_2.edit().putBoolean("isRemembered", binding.ifRememberBox.isChecked()).apply();
+
+
+
+                    Intent intent = new Intent(MainActivity.this, FindingRingAndRidePage.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Utilities.showToast(MainActivity.this, e.getLocalizedMessage());
+                }
+            });
+        }
 
 
     }
@@ -50,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void forgotPasswordTextClicked(View view){
 
-        System.out.println("hello");
+
     }
 
 
