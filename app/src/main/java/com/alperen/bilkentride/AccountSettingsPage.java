c0a67f7e938079964a4a6c427e77dd388d45d576
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-26 21:06:48 +0300

    XML
    Account_settings_page xml is done.

---
+++ app/src/main/java/com/alperen/bilkentride/AccountSettingsPage.java
@@ -0,0 +1,49 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountSettingsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings_page);
+    }
+
+    public void editProfileButtonClicked()
+    {
+
+    }
+
+    public void passwordButtonClicked()
+    {
+
+    }
+
+    public void preferencesButtonClicked()
+    {
+
+    }
+
+    public void logOutButtonClicked()
+    {
+
+    }
+
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
+}
