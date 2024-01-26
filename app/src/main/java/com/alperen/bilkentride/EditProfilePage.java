98de41cebf5f2d7ec7735bbd1deb079f18acb2d4
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-26 23:50:38 +0300

    XML
    edit_profile_page xml is done (except "are you rider" part and save changes button color)

---
+++ app/src/main/java/com/alperen/bilkentride/EditProfilePage.java
@@ -0,0 +1,44 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_page);
+    }
+
+    public void backButtonClicked()
+    {
+
+    }
+
+    public void editProfilePhotoButtonClicked()
+    {
+
+    }
+
+    public void saveChangesButtonClicked()
+    {
+
+    }
+
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
+}
