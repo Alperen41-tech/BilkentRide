e2f742916ac1d1682ede5ea9ec5d940b0f8024cd
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-28 00:51:06 +0300

    XML
    dialog_page xml is done

---
+++ app/src/main/java/com/alperen/bilkentride/DialogPage.java
@@ -0,0 +1,38 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialogPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_page);
+    }
+
+    public void backButtonClicked()
+    {
+
+    }
+
+    public void sendMessageButtonClicked()
+    {
+        
+    }
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
+}
