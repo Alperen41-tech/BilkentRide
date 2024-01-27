9af596812a4200206d890a2f11aa431a719da984
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-27 22:32:25 +0300

    XML
    security_page xml is done (except the color of save new password button)

---
+++ app/src/main/java/com/alperen/bilkentride/SecurityPage.java
@@ -0,0 +1,24 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecurityPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_page);
+    }
+
+    public void backButtonClicked()
+    {
+
+    }
+
+    public void saveNewPasswordButtonClicked()
+    {
+
+    }
+}
