194b20b61c5f6672f7f14d8718fe9c06a15d7d05
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-01-29 15:45:42 +0300

    XML
    making_ride_page xml is done

---
+++ app/src/main/java/com/alperen/bilkentride/MakingRidePage.java
@@ -0,0 +1,49 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakingRidePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_making_ride_page);
+    }
+
+    public void goingBack_MainPage()
+    {
+
+    }
+
+    public void locationButtonClicked()
+    {
+
+    }
+
+    public void mapButtonClicked()
+    {
+
+    }
+
+    public void letsFindTravellerButtonClicked()
+    {
+        
+    }
+
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
+}
