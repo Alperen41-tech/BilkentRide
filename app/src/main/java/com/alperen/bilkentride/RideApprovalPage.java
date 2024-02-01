f240c7f8008416cfbb92bfcf894ccdc99ee0d9f2
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-02-01 12:27:07 +0300

    XML
    ride_approval_page xml is done

---
+++ app/src/main/java/com/alperen/bilkentride/RideApprovalPage.java
@@ -0,0 +1,34 @@
+package com.alperen.bilkentride;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RideApprovalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_approval_page);
+    }
+
+    public void backButtonClicked()
+    {
+
+    }
+
+    public void chatButtonClicked()
+    {
+
+    }
+
+    public void mainPageButtonClicked()
+    {
+
+    }
+
+    public void profileButtonClicked()
+    {
+
+    }
+}
